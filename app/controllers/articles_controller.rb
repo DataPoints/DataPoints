1aaad1acfe51a92c7a79c013396991f21fb458f4
Peter Uherek <peteruherek@gmail.com>
2014-10-15 21:16:22 +0200

    Pridany format articlov

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def edit
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
+  def update
+     @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
   def create
     @article = Article.new(article_params)
 
@@ -16,7 +27,6 @@ class ArticlesController < ApplicationController
     end
 
   end
-
 
 
   def show
