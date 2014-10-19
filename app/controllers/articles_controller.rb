368d9802e9fac9b8c18a051f01bf247cadfc6e21
Peter Uherek <peteruherek@gmail.com>
2014-10-19 15:21:44 +0200

    --

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,13 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
+  
   def edit
     @article = Article.find(params[:id])
   end
