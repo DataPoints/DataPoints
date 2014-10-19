676fbc0bdbcaaf04d0f9547e8cc20e58f855466a
AlexOstrovsky <axll.ostrovsky@gmail.com>
2014-10-19 21:25:15 +0200

    Added association with userID (uploading datasets)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018214618) do
+ActiveRecord::Schema.define(version: 20141019192148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 20141018214618) do
     t.string   "description"
     t.string   "link"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
+  add_index "datasets", ["user_id", "created_at"], name: "index_datasets_on_user_id_and_created_at", using: :btree
   add_index "datasets", ["user_id"], name: "index_datasets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141018214618) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
