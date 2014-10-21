388d019fe8cdf13d087a1565b9adfe69be387cd7
Peter Uherek <peteruherek@gmail.com>
2014-10-21 23:04:50 +0200

    DATAPOINT-8,10

--- app/assets/javascripts/datasets.js.coffee
+++ app/assets/javascripts/datasets.js.coffee
@@ -1,3 +1,17 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready set = ->
+  $("#showmodal").on "click", ->
+    $("#myModal").modal "toggle"
+    return
+
+  $("#showmodal2").on "click", ->
+    $("#myModal").modal "toggle"
+    return
+
+  $("#showmodal2").on "click", ->
+    $("#myModal").modal "toggle"
+    return
+
+  return
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018214618) do
+ActiveRecord::Schema.define(version: 20141019205934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,14 @@ ActiveRecord::Schema.define(version: 20141018214618) do
     t.string   "description"
     t.string   "link"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "status"
+    t.boolean  "deleted",     default: false
+    t.string   "storage"
   end
 
+  add_index "datasets", ["user_id", "created_at"], name: "index_datasets_on_user_id_and_created_at", using: :btree
   add_index "datasets", ["user_id"], name: "index_datasets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
