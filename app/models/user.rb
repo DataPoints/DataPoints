f314ea1cfaea53c37244295edab0cf593d466e13
martinlosak <martinlosak2@gmail.com>
2014-10-22 20:40:50 +0200

    Mensie zmeny pri Dataset Upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :datasets
+  has_many :datasets, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
