e0186f1a10fd2847fd7e93912af6300d579f1b34
AlexOstrovsky <axll.ostrovsky@gmail.com>
2014-11-15 10:39:32 +0100

    Added: Registration email confirmation, Forgot password feature and Recaptcha

--- Gemfile
+++ Gemfile
@@ -41,3 +41,6 @@ gem 'spring'
 gem 'bcrypt'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
+
+gem "recaptcha", :require => "recaptcha/rails"
+gem 'jquery-turbolinks'
--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,11 +3,11 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      flash[:success] = "Account activated!"
+      flash[:success] = 'Account activated!'
       log_in user
       redirect_to root_url
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = 'Invalid activation link'
       redirect_to root_url
     end
   end
