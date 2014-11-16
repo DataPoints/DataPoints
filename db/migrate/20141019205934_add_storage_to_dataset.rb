19cc6dab97239ba7fb7fbb40cef709e0c29e769a
Peter Uherek <peteruherek@gmail.com>
2014-11-16 18:10:16 +0100

    305 Stiahnutie datasetu a pridanie do DB - Uprava tabuliek

--- db/migrate/20141019205934_add_storage_to_dataset.rb
+++ db/migrate/20141019205934_add_storage_to_dataset.rb
@@ -1,6 +1,5 @@
 class AddStorageToDataset < ActiveRecord::Migration
   def change
-    add_column :datasets, :data_table, :string
-    add_column :datasets, :header_table, :string
+    add_column :datasets, :data_table_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,10 @@ ActiveRecord::Schema.define(version: 20141113201042) do
     t.integer  "user_id"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
-    t.string   "status"
+    t.string   "status" 
     t.boolean  "deleted",                       default: false
-    t.string   "data_table"
+    t.string   "data_table_name"
     t.decimal  "analyzed_progress",             default: 0.0
-    t.string   "header_table",      limit: 250
   end
 
   add_index "datasets", ["user_id", "created_at"], name: "index_datasets_on_user_id_and_created_at", using: :btree
