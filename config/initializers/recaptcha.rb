d8275fd80d4b3d948d1fd84de9bf30ef683d36f2
Alex Ostrovsky <axll.ostrovsky@gmail.com>
2014-11-18 12:03:55 +0100

    Update recaptcha.rb
    doplnenie komentara

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,4 @@
+# Nastavenie configuracie klucov pre recaptchu
 Recaptcha.configure do |config|
   config.public_key = '6LcDi_0SAAAAAFVK4MvRWQUL532mD17mlvqK2tFa'
   config.private_key = '6LcDi_0SAAAAAIDZOiNvsmn9z5xct2YwakuDjGyg'
