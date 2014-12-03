98fed9fb8fd41af7c25be82dde0286a6ff26205e
Peter Uherek <peteruherek@gmail.com>
2014-12-03 23:41:41 +0100

    66:401 Ako pouzivatel chcem spustit analyzu dat - Oprava routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'datasets/new'
   get 'datasets/change_type' => 'datasets#change_type'
+  get 'datasets/start_analyze' => 'datasets#start_analyze'
   get 'datasets/show' =>'datasets#show'
   get 'home/index'
   get 'signup' => 'users#new'
